a46df9ad8f6c53f437b25a8b38840a6ed495620d
Mohamed Mohy <105385056+MohamedMohy0@users.noreply.github.com>
2025-05-15 20:45:45 +0300

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,16 @@ st.set_page_config(
      page_icon="logo.jpg",
     layout="centered"
 )
-st.image("logo.jpg", width=120, caption="الشعار")
+st.image("logo.jpg", width=120 )
+st.markdown(
+    """
+    <div style='text-align: center; padding: 20px;'>
+        <img src='logo.png' style='width:120px; height:120px; border-radius:50%; border: 2px solid #4e8f69;' />
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
 # إعداد الاتصال بـ Google Sheet
 @st.cache_resource
 def connect_to_sheet():
